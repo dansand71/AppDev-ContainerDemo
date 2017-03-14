587c33c9dc581f72f7f98153572166cddb738f89
dansand71 <dansand71@hotmail.com>
2017-03-14 17:24:41 -0400

    88

--- 1-SetupDemo.sh
+++ 1-SetupDemo.sh
@@ -79,14 +79,14 @@ echo "--------------------------------------------"
 echo "Configure demo scripts"
 sudo echo "export ANSIBLE_HOST_KEY_CHECKING=false" >> ~/.bashrc
 export ANSIBLE_HOST_KEY_CHECKING=false
-sudo sed -i -e "s@VALUEOF-UNIQUE-SERVER-PREFIX@$DEMO_UNIQUE_SERVER_PREFIX@g" /source/AppDev-ContainerDemo/.
-sudo sed -i -e "s@VALUEOF-UNIQUE-STORAGE-ACCOUNT-PREFIX@$DEMO_STORAGE_ACCOUNT@g" /source/AppDev-ContainerDemo/.
-sudo sed -i -e "s@VALUEOF-REGISTRY-SERVER-NAME@$DEMO_REGISTRY_SERVER_NAME@g" /source/AppDev-ContainerDemo/.
-sudo sed -i -e "s@VALUEOF-REGISTRY-USER-NAME@$DEMO_REGISTRY_USER_NAME@g" /source/AppDev-ContainerDemo/.
-sudo sed -i -e "s@VALUEOF-REGISTRY-PASSWORD@$DEMO_REGISTRY_PASSWORD@g" /source/AppDev-ContainerDemo/.
-sudo sed -i -e "s@VALUEOF-REPLACE-OMS-WORKSPACE@$DEMO_OMS_WORKSPACE@g" /source/AppDev-ContainerDemo/.
-sudo sed -i -e "s@VALUEOF-REPLACE-OMS-PRIMARYKEY@$DEMO_OMS_PRIMARYKEY@g" /source/AppDev-ContainerDemo/.
-sudo sed -i -e "s@VALUEOF-APPLICATION-INSIGHTS-KEY@$DEMO_APPLICATION_INSIGHTS_KEY@g" /source/AppDev-ContainerDemo/.
+sudo grep -rl VALUEOF-UNIQUE-SERVER-PREFIX ./source/AppDev-ContainerDemo  | sudo xargs sed -i -e "s@VALUEOF-UNIQUE-SERVER-PREFIX@$DEMO_UNIQUE_SERVER_PREFIX@g"
+sudo grep -rl VALUEOF-UNIQUE-STORAGE-ACCOUNT-PREFIX ./source/AppDev-ContainerDemo  | sudo sed -i -e "s@VALUEOF-UNIQUE-STORAGE-ACCOUNT-PREFIX@$DEMO_STORAGE_ACCOUNT@g" /source/AppDev-ContainerDemo/.
+sudo grep -rl VALUEOF-REGISTRY-SERVER-NAME ./source/AppDev-ContainerDemo  | sudo sed -i -e "s@VALUEOF-REGISTRY-SERVER-NAME@$DEMO_REGISTRY_SERVER_NAME@g" /source/AppDev-ContainerDemo/.
+sudo grep -rl VALUEOF-REGISTRY-USER-NAME ./source/AppDev-ContainerDemo  | sudo sed -i -e "s@VALUEOF-REGISTRY-USER-NAME@$DEMO_REGISTRY_USER_NAME@g" /source/AppDev-ContainerDemo/.
+sudo grep -rl VALUEOF-REGISTRY-PASSWORD ./source/AppDev-ContainerDemo  | sudo sed -i -e "s@VALUEOF-REGISTRY-PASSWORD@$DEMO_REGISTRY_PASSWORD@g" /source/AppDev-ContainerDemo/.
+sudo grep -rl VALUEOF-REPLACE-OMS-WORKSPACE ./source/AppDev-ContainerDemo  | sudo sed -i -e "s@VALUEOF-REPLACE-OMS-WORKSPACE@$DEMO_OMS_WORKSPACE@g" /source/AppDev-ContainerDemo/.
+sudo grep -rl VALUEOF-REPLACE-OMS-PRIMARYKEY ./source/AppDev-ContainerDemo  | sudo sed -i -e "s@VALUEOF-REPLACE-OMS-PRIMARYKEY@$DEMO_OMS_PRIMARYKEY@g" /source/AppDev-ContainerDemo/.
+sudo grep -rl VALUEOF-APPLICATION-INSIGHTS-KEY ./source/AppDev-ContainerDemo  | sudo sed -i -e "s@VALUEOF-APPLICATION-INSIGHTS-KEY@$DEMO_APPLICATION_INSIGHTS_KEY@g" /source/AppDev-ContainerDemo/.
 
 echo ""
 echo"---------------------------------------------"
