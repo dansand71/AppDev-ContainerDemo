982b2896a75130f0e667ddb21734cd9c4631854a
dansand71 <dansand71@hotmail.com>
2017-03-22 14:48:13 -0400

    125

---
+++ sample-apps/eShopOnContainers/src/Web/WebMVC/Services/CatalogService.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.eShopOnContainers.WebMVC.ViewModels;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.Extensions.Options;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.eShopOnContainers.WebMVC.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly IOptionsSnapshot<AppSettings> _settings;
+        private HttpClient _apiClient;
+        private readonly string _remoteServiceBaseUrl;
+  
+        public CatalogService(IOptionsSnapshot<AppSettings> settings, ILoggerFactory loggerFactory) {
+            _settings = settings;
+            _remoteServiceBaseUrl = $"{_settings.Value.CatalogUrl}/api/v1/catalog/";
+
+            var log = loggerFactory.CreateLogger("catalog service");
+            log.LogDebug(settings.Value.CatalogUrl);
+        }
+         
+        public async Task<Catalog> GetCatalogItems(int page,int take, int? brand, int? type)
+        {
+            _apiClient = new HttpClient();
+            var itemsQs = $"items?pageIndex={page}&pageSize={take}";
+            var filterQs = "";
+
+            if (brand.HasValue || type.HasValue)
+            {
+                var brandQs = (brand.HasValue) ? brand.Value.ToString() : "null";
+                var typeQs = (type.HasValue) ? type.Value.ToString() : "null";
+                filterQs = $"/type/{typeQs}/brand/{brandQs}";
+            }
+
+            var catalogUrl = $"{_remoteServiceBaseUrl}items{filterQs}?pageIndex={page}&pageSize={take}";
+
+            var dataString = "";
+
+            //
+            // Using HttpClient with Retry and Exponential Backoff
+            //
+            var retry = new RetryWithExponentialBackoff();
+            await retry.RunAsync(async () =>
+            {
+                // work with HttpClient call
+                dataString = await _apiClient.GetStringAsync(catalogUrl);
+            });
+
+            //var dataString = await _apiClient.GetStringAsync(catalogUrl);
+            var response = JsonConvert.DeserializeObject<Catalog>(dataString);
+
+            return response;
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetBrands()
+        {
+            _apiClient = new HttpClient();
+            var url = $"{_remoteServiceBaseUrl}catalogBrands";
+            var dataString = await _apiClient.GetStringAsync(url);
+
+            var items = new List<SelectListItem>();
+            items.Add(new SelectListItem() { Value = null, Text = "All", Selected = true });
+
+            JArray brands = JArray.Parse(dataString);
+            foreach (JObject brand in brands.Children<JObject>())
+            {
+                dynamic item = brand;
+                items.Add(new SelectListItem() { Value = item.id, Text = item.brand });
+            }
+
+            return items;
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetTypes()
+        {
+            _apiClient = new HttpClient();
+            var url = $"{_remoteServiceBaseUrl}catalogTypes";
+            var dataString = await _apiClient.GetStringAsync(url);
+
+            var items = new List<SelectListItem>();
+            items.Add(new SelectListItem() { Value = null, Text = "All", Selected = true });
+
+            JArray brands = JArray.Parse(dataString);
+            foreach (JObject brand in brands.Children<JObject>())
+            {
+                dynamic item = brand;
+                items.Add(new SelectListItem() { Value = item.id, Text = item.type });
+            }
+
+            return items;
+        }
+    }
+}
---
+++ sample-apps/eShopOnContainers/src/Web/WebMVC/Services/OrderingService.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.eShopOnContainers.WebMVC.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Microsoft.eShopOnContainers.WebMVC.Services
+{
+    public class OrderingService : IOrderingService
+    {
+        private HttpClient _apiClient;
+        private readonly string _remoteServiceBaseUrl;
+        private readonly IOptionsSnapshot<AppSettings> _settings;
+        private readonly IHttpContextAccessor _httpContextAccesor;
+
+        public OrderingService(IOptionsSnapshot<AppSettings> settings, IHttpContextAccessor httpContextAccesor)
+        {
+            _remoteServiceBaseUrl = $"{settings.Value.OrderingUrl}/api/v1/orders";
+            _settings = settings;
+            _httpContextAccesor = httpContextAccesor;
+        }
+
+        async public Task<Order> GetOrder(ApplicationUser user, string Id)
+        {
+            var context = _httpContextAccesor.HttpContext;
+            var token = await context.Authentication.GetTokenAsync("access_token");
+
+            _apiClient = new HttpClient();
+            _apiClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            var ordersUrl = $"{_remoteServiceBaseUrl}/{Id}";
+            var dataString = await _apiClient.GetStringAsync(ordersUrl);
+            
+            var response = JsonConvert.DeserializeObject<Order>(dataString);
+
+            return response;
+        }
+
+        async public Task<List<Order>> GetMyOrders(ApplicationUser user)
+        {
+            var context = _httpContextAccesor.HttpContext;
+            var token = await context.Authentication.GetTokenAsync("access_token");
+
+            _apiClient = new HttpClient();
+            _apiClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            var ordersUrl = _remoteServiceBaseUrl;
+            var dataString = await _apiClient.GetStringAsync(ordersUrl);
+            var response = JsonConvert.DeserializeObject<List<Order>>(dataString);
+
+            return response; 
+        }
+
+        public Order MapUserInfoIntoOrder(ApplicationUser user, Order order)
+        {
+            order.City = user.City;
+            order.Street = user.Street;
+            order.State = user.State;
+            order.Country = user.Country;
+            order.ZipCode = user.ZipCode;
+                
+            order.CardNumber = user.CardNumber;
+            order.CardHolderName = user.CardHolderName;
+            order.CardExpiration = new DateTime(int.Parse("20" + user.Expiration.Split('/')[1]),int.Parse(user.Expiration.Split('/')[0]), 1);
+            order.CardSecurityNumber = user.SecurityNumber;
+
+            return order;
+        }
+
+        async public Task CreateOrder(Order order)
+        {
+            var context = _httpContextAccesor.HttpContext;
+            var token = await context.Authentication.GetTokenAsync("access_token");
+
+            _apiClient = new HttpClient();
+            _apiClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            _apiClient.DefaultRequestHeaders.Add("x-requestid", order.RequestId.ToString());
+            var ordersUrl = $"{_remoteServiceBaseUrl}/new";
+            order.CardTypeId = 1;
+            order.CardExpirationApiFormat();
+            SetFakeIdToProducts(order);
+            
+            StringContent content = new StringContent(JsonConvert.SerializeObject(order), System.Text.Encoding.UTF8, "application/json");
+           
+            var response = await _apiClient.PostAsync(ordersUrl, content);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+                throw new Exception("Error creating order, try later");
+        }
+
+        public void OverrideUserInfoIntoOrder(Order original, Order destination)
+        {
+            destination.City = original.City;
+            destination.Street = original.Street;
+            destination.State = original.State;
+            destination.Country = original.Country;
+            destination.ZipCode = original.ZipCode;
+
+            destination.CardNumber = original.CardNumber;
+            destination.CardHolderName = original.CardHolderName;
+            destination.CardExpiration = original.CardExpiration;
+            destination.CardSecurityNumber = original.CardSecurityNumber;
+        }
+
+        private void SetFakeIdToProducts(Order order)
+        {
+            var id = 1;
+            order.OrderItems.ForEach(x => { x.ProductId = id; id++; });
+        }
+    }
+}
