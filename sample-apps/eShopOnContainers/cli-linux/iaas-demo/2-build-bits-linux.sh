5334e554599fc15b8f311d9d2490ffc52fe2df00
dansand71 <dansand71@hotmail.com>
2017-03-31 14:07:42 -0400

    asf
    adf

--- sample-apps/eShopOnContainers/cli-linux/iaas-demo/2-build-bits-linux.sh
+++ sample-apps/eShopOnContainers/cli-linux/iaas-demo/2-build-bits-linux.sh
@@ -31,10 +31,10 @@ echo ""
 echo "Remove any existing eShop docker images on the build box...."
 images=$(sudo docker images --filter=reference="eshop/*" -q)
 if [ -n "$images" ]; then
-    docker rm $(sudo docker ps -a -q) -f
+    sudo docker rm $(sudo docker ps -a -q) -f
     echo "Deleting eShop images in local Docker repo"
     echo $images
-    docker rmi $(sudo docker images --filter=reference="eshop/*" -q) -f
+    sudo docker rmi $(sudo docker images --filter=reference="eshop/*" -q) -f
 fi
 
 # No need to build the images, docker build or docker compose will
