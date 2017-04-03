95285ec4b3511bf94e4b93e337153b82c1c688b7
dansand71 <dansand71@hotmail.com>
2017-04-03 19:45:47 -0400

    a

--- sample-apps/aspnet-core-linux/setupdemo/paas-demo/1-setup-demo.sh
+++ sample-apps/aspnet-core-linux/setupdemo/paas-demo/1-setup-demo.sh
@@ -5,14 +5,14 @@ az appservice plan create -g ossdemo-appdev-paas -n webtier-plan --is-linux --nu
 az appservice web create -g ossdemo-appdev-paas -p webtier-plan -n VALUEOF-UNIQUE-SERVER-PREFIX-aspnet-core-linux-paas
 
 ## Config the Docker Container
-az appservice web config container update -n aspnet-core-linux-paas -g ossdemo-appdev-paas \
+az appservice web config container update -n VALUEOF-UNIQUE-SERVER-PREFIX-aspnet-core-linux-paas -g ossdemo-appdev-paas \
     --docker-registry-server-password VALUEOF-REGISTRY-PASSWORD \
     --docker-registry-server-user VALUEOF-REGISTRY-USER-NAME \
     --docker-registry-server-url VALUEOF-REGISTRY-SERVER-NAME \
     --docker-custom-image-name VALUEOF-REGISTRY-SERVER-NAME/gbbossdemo/aspnet-core-linux
 
 ## Config ARR so we round robin for demos
-az appservice web config container update -n aspnet-core-linux-paas -g ossdemo-appdev-paas \
+az appservice web config container update -n VALUEOF-UNIQUE-SERVER-PREFIX-aspnet-core-linux-paas -g ossdemo-appdev-paas \
     --docker-registry-server-password VALUEOF-REGISTRY-PASSWORD \
     --docker-registry-server-user VALUEOF-REGISTRY-USER-NAME \
     --docker-registry-server-url VALUEOF-REGISTRY-SERVER-NAME \
