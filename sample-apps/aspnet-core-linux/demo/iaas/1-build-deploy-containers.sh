f608673273af0c9671367ce6b82c8fedb2060949
dansand71 <dansand71@hotmail.com>
2017-04-07 13:54:39 -0400

    as

--- sample-apps/aspnet-core-linux/demo/iaas/1-build-deploy-containers.sh
+++ sample-apps/aspnet-core-linux/demo/iaas/1-build-deploy-containers.sh
@@ -1,4 +1,6 @@
 echo "This script leverages Ansible to deploy the new docker host to the VM's created in step 1'"
 #az account set --subscription "Microsoft Azure Internal Consumption"
 
-ansible-playbook -i hosts ansible-docker-publish.yml
+#change into the directory where the Ansible CFG is located
+cd /source/AppDev-ContainerDemo/sample-apps/aspnet-core-linux/demo/iaas
+ansible-playbook -i /source/AppDev-ContainerDemo/environment/iaas/hosts ansible-docker-publish.yml
