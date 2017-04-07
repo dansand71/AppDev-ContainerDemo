1a3e2a059e0c2679c609b4b9f71f66cabac334dc
dansand71 <dansand71@hotmail.com>
2017-04-07 16:02:19 -0400

    final push

--- environment/paas/create-webplan-linux-service.sh
+++ environment/paas/create-webplan-linux-service.sh
@@ -5,3 +5,11 @@ echo ".creating appservice web plan"
 echo ".creating appservice web app"
 ## Create the appservice - Already done via template
 ~/bin/az appservice web create -g ossdemo-appdev-paas -p webtier-plan -n VALUEOF-UNIQUE-SERVER-PREFIX-aspnet-core-linux-paas
+
+echo ".updating the web app with the container details"
+## Config the Docker Container
+~/bin/az appservice web config container update -n dsand71-aspnet-core-linux-paas -g ossdemo-appdev-paas \
+    --docker-registry-server-password +DMd=zRH+i+LnW0m173F=9vFq54ngWPs \
+    --docker-registry-server-user dsand71demoregistry \
+    --docker-registry-server-url dsand71demoregistry.azurecr.io \
+    --docker-custom-image-name dsand71demoregistry.azurecr.io/ossdemo/aspnet-core-linux
--- sample-apps/aspnet-core-linux/demo/paas/configure-container-settings.sh
+++
@@ -1,6 +0,0 @@
-## Config the Docker Container
-~/bin/az appservice web config container update -n VALUEOF-UNIQUE-SERVER-PREFIX-aspnet-core-linux-paas -g ossdemo-appdev-paas \
-    --docker-registry-server-password VALUEOF-REGISTRY-PASSWORD \
-    --docker-registry-server-user VALUEOF-REGISTRY-USER-NAME \
-    --docker-registry-server-url VALUEOF-REGISTRY-SERVER-NAME \
-    --docker-custom-image-name VALUEOF-REGISTRY-SERVER-NAME/ossdemo/aspnet-core-linux
