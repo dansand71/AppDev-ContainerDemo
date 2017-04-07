c0571dd8d3febb488b9572d524e00087aed79288
dansand71 <dansand71@hotmail.com>
2017-04-07 14:37:59 -0400

    final fixes

--- sample-apps/aspnet-core-linux/demo/paas/configure-container-settings.sh
+++ sample-apps/aspnet-core-linux/demo/paas/configure-container-settings.sh
@@ -1,14 +1,6 @@
 ## Config the Docker Container
-az appservice web config container update -n VALUEOF-UNIQUE-SERVER-PREFIX-aspnet-core-linux-paas -g ossdemo-appdev-paas \
+~/bin/az appservice web config container update -n VALUEOF-UNIQUE-SERVER-PREFIX-aspnet-core-linux-paas -g ossdemo-appdev-paas \
     --docker-registry-server-password VALUEOF-REGISTRY-PASSWORD \
     --docker-registry-server-user VALUEOF-REGISTRY-USER-NAME \
     --docker-registry-server-url VALUEOF-REGISTRY-SERVER-NAME \
-    --docker-custom-image-name VALUEOF-REGISTRY-SERVER-NAME/gbbossdemo/aspnet-core-linux
-
-## Config ARR so we round robin for demos
-az appservice web config container update -n VALUEOF-UNIQUE-SERVER-PREFIX-aspnet-core-linux-paas -g ossdemo-appdev-paas \
-    --docker-registry-server-password VALUEOF-REGISTRY-PASSWORD \
-    --docker-registry-server-user VALUEOF-REGISTRY-USER-NAME \
-    --docker-registry-server-url VALUEOF-REGISTRY-SERVER-NAME \
-    --docker-custom-image-name VALUEOF-REGISTRY-SERVER-NAME/gbbossdemo/aspnet-core-linux
-
+    --docker-custom-image-name VALUEOF-REGISTRY-SERVER-NAME/ossdemo/aspnet-core-linux
