8174736255815ae62d2afde94a6191512b693313
dansand71 <dansand71@hotmail.com>
2017-04-07 11:01:29 -0400

    asf

--- environment/acs/deploy-k8s-OMSDaemonset.sh
+++ environment/acs/deploy-k8s-OMSDaemonset.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "Deploy the OMS Daemonset to k8s for monitoring"
+cd /source/AppDev-ContainerDemo/environment/acs
+
 ~/bin/kubectl create -f OMSDaemonset.yml
--- environment/iaas/deploy-docker-engine.sh
+++ environment/iaas/deploy-docker-engine.sh
@@ -1,5 +1,5 @@
 echo "Calling Ansible to deploy the new docker engine to the VM's'"
 #az account set --subscription "Microsoft Azure Internal Consumption"
 
-cd /source/AppDev-ContainerDemo/environment
+cd /source/AppDev-ContainerDemo/environment/iaas
 ansible-playbook -i hosts deploy-dockerengine-playbook.yml
