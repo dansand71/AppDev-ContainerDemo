685b1da2f9b25c1bf3cae80ba4e6864f64aed4ee
dansand71 <dansand71@hotmail.com>
2017-04-08 17:23:28 -0400

    dos2unix for all json files

--- environment/set-scripts-executable.sh
+++ environment/set-scripts-executable.sh
@@ -1,3 +1,6 @@
 #Set Scripts as executable
 echo ".setting scripts as executables"
 find .  -type f -name "*.sh" -exec sudo chmod +x {} \;
+
+find .  -type f -name "*.sh" -exec sudo dos2unix {} \;
+find .  -type f -name "*.json" -exec sudo dos2unix {} \;
