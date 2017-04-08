fc5128c6f62a886786b5ebd46dfdcef29f945163
dansand71 <dansand71@hotmail.com>
2017-04-08 18:08:19 -0400

    generalize for deployment

--- environment/paas/create-webplan-linux-service.sh
+++ environment/paas/create-webplan-linux-service.sh
@@ -8,8 +8,8 @@ echo ".creating appservice web app"
 
 echo ".updating the web app with the container details"
 ## Config the Docker Container
-~/bin/az appservice web config container update -n dsand71-aspnet-core-linux-paas -g ossdemo-appdev-paas \
-    --docker-registry-server-password +DMd=zRH+i+LnW0m173F=9vFq54ngWPs \
-    --docker-registry-server-user dsand71demoregistry \
-    --docker-registry-server-url dsand71demoregistry.azurecr.io \
-    --docker-custom-image-name dsand71demoregistry.azurecr.io/ossdemo/aspnet-core-linux
+~/bin/az appservice web config container update -n VALUEOF-UNIQUE-SERVER-PREFIX-aspnet-core-linux-paas -g ossdemo-appdev-paas \
+    --docker-registry-server-password VALUEOF-REGISTRY-PASSWORD \
+    --docker-registry-server-user VALUEOF-REGISTRY-USER-NAME \
+    --docker-registry-server-url VALUEOF-REGISTRY-SERVER-NAME \
+    --docker-custom-image-name VALUEOF-REGISTRY-SERVER-NAME/ossdemo/aspnet-core-linux
