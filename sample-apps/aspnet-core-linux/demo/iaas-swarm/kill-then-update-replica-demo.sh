83f9a0141dda1fb70f3b43556a51a560c454d152
dansand71 <dansand71@hotmail.com>
2017-04-10 17:31:23 -0400

    SWARM v2

--- sample-apps/aspnet-core-linux/demo/iaas-swarm/kill-then-update-replica-demo.sh
+++ sample-apps/aspnet-core-linux/demo/iaas-swarm/kill-then-update-replica-demo.sh
@@ -3,4 +3,10 @@
 # we are going to kill that running containerby command
 
 #service name = replica count.  To get service name run "docker service ls""
-docker service scale aspnet_web=5
+sudo docker service scale aspnet_web=5
+
+#to remove the service
+#sudo docker service rm aspnet_web
+
+#to list the detials
+#sudo docker service ps aspnet_web
