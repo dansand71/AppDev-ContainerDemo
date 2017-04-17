9d4e820c68c77b98dcd7a4bb173d8d60bfb9c67a
dansand71 <dansand71@hotmail.com>
2017-04-17 18:37:49 -0400

    SWARM cluster with SQL DB

--- sample-apps/eShopOnContainers/cli-linux/iaas-demo/1-create-swarm-cluster.sh
+++ sample-apps/eShopOnContainers/cli-linux/iaas-demo/1-create-swarm-cluster.sh
@@ -1,3 +1,6 @@
 /source/AppDev-ContainerDemo/environment/iaas/create-swarm-cluster.sh
 
 sudo docker stack deploy --compose-file visualizer.yml monitoring
+
+#this is to show a temporary scenario that we store data from the linux container into a local directory on the jumpbox server
+mkdir /source/AppDev-ContainerDemo/sample-apps/eShopOnContainers/cli-linux/iaas-demo/sqldata
--- sample-apps/eShopOnContainers/cli-linux/paas-demo/1-create-swarm-cluster-sqldb.sh
+++ sample-apps/eShopOnContainers/cli-linux/paas-demo/1-create-swarm-cluster-sqldb.sh
@@ -1,6 +1,11 @@
-echo "Creating SWARM Cluster."
-/source/AppDev-ContainerDemo/environment/iaas/create-swarm-cluster.sh
+read -p "Create SWARM Cluster? [Y/n]:"  continuescript
+if [[ ${continuescript,,} != "n" ]];then
+  echo "Creating SWARM Cluster."
+  /source/AppDev-ContainerDemo/environment/iaas/create-swarm-cluster.sh
+fi
 
+read -p "Create SQL PAAS Server? [Y/n]:"  continuescript
+if [[ ${continuescript,,} != "n" ]];then
 echo "Creating SQL Server in ossdemo-utility resource group."
 ### SQL SERVER PASSWORD
 while true
@@ -18,9 +23,11 @@ do
 done
 
 ~/bin/az sql server create --name ossdemo-sql-VALUEOF-UNIQUE-SERVER-PREFIX --resource-group ossdemo-utility --location eastus \
-    --admin-user VALUEOF-DEMO-ADMIN-USER-NAME --admin-password $NEWPASSWORD
+    --admin-user VALUEOF-DEMO-ADMIN-USER-NAME --admin-password $NEWPassword
 
 echo "Replacing values in the STACK deployment file."
 sed -i -e "s@REPLACESQLADMIN@VALUEOF-DEMO-ADMIN-USER-NAME@g" docker-stack.yml
-sed -i -e "s@REPLACESQLPASSWORD@$NEWPASSWORD@g" docker-stack.yml
+sed -i -e "s@REPLACESQLPASSWORD@$NEWPassword@g" docker-stack.yml
 sed -i -e "s@REPLACESQLSERVER@ossdemo-sql-VALUEOF-UNIQUE-SERVER-PREFIX.database.windows.net@g" docker-stack.yml
+
+fi
