71d5d6299482723d5e3aa934f7b3f5a0d8a04716
dansand71 <dansand71@hotmail.com>
2017-04-18 07:51:36 -0400

    Added SQL Server Firewall Rule

--- sample-apps/eShopOnContainers/cli-linux/paas-demo/1-create-swarm-cluster-sqldb.sh
+++ sample-apps/eShopOnContainers/cli-linux/paas-demo/1-create-swarm-cluster-sqldb.sh
@@ -21,9 +21,13 @@ do
      echo -e ".${RED}Passwords do not match.  Please retry. ${RESET}"
   fi
 done
-
+echo ".verified password.  Running command to create SQL Server."
 ~/bin/az sql server create --name ossdemo-sql-VALUEOF-UNIQUE-SERVER-PREFIX --resource-group ossdemo-utility --location eastus \
     --admin-user VALUEOF-DEMO-ADMIN-USER-NAME --admin-password $NEWPassword
+
+echo ".allowing Azure traffic to the new SQL Server"
+~/bin/az sql server firewall-rule create -g ossdemo-utility -s ossdemo-sql-VALUEOF-UNIQUE-SERVER-PREFIX --end-ip-address "0.0.0.0" --name AllowAllWindowsAzureIps --start-ip-address "0.0.0.0"
+
 
 echo "Replacing values in the STACK deployment file."
 sed -i -e "s@REPLACESQLADMIN@VALUEOF-DEMO-ADMIN-USER-NAME@g" docker-stack.yml
