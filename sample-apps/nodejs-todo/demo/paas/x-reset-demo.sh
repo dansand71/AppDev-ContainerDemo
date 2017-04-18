28bd345659f0f2896e23dc7af29fe487fca222e0
dansand71 <dansand71@hotmail.com>
2017-04-18 19:03:19 -0400

    npm install app insights

--- sample-apps/nodejs-todo/demo/paas/x-reset-demo.sh
+++ sample-apps/nodejs-todo/demo/paas/x-reset-demo.sh
@@ -2,4 +2,4 @@
 echo "Delete the service and deployment which will cleanup the pods and external IP's'"
 #az account set --subscription "Microsoft Azure Internal Consumption"
 kubectl delete service nodejs-todo-with-documentdb
-kubectl delete deployment nodejs-todo-deployment-with-documentdb
+kubectl delete deployment nodejs-todo-with-documentdb-deployment
