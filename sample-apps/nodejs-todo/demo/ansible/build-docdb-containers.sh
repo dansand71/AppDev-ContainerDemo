5466d7460849272d2fdced3a24a9f12d14ae0a92
dansand71 <dansand71@hotmail.com>
2017-04-18 18:49:14 -0400

    tweaks on image name

--- sample-apps/nodejs-todo/demo/ansible/build-docdb-containers.sh
+++ sample-apps/nodejs-todo/demo/ansible/build-docdb-containers.sh
@@ -1,4 +1,4 @@
-echo "This script builds and deploys the aspnet-core-linux application into a container and places it into the Azure Registry."
+echo "This script builds and deploys the nodejs with docdb application into a container and places it into the Azure Registry."
 #az account set --subscription "Microsoft Azure Internal Consumption"
 
 #change into the directory where the Ansible CFG is located
--- sample-apps/nodejs-todo/demo/paas/x-reset-demo.sh
+++ sample-apps/nodejs-todo/demo/paas/x-reset-demo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Delete the service and deployment which will cleanup the pods and external IP's'"
 #az account set --subscription "Microsoft Azure Internal Consumption"
-kubectl delete service nodejs-todo
-kubectl delete deployment nodejs-todo-deployment
+kubectl delete service nodejs-todo-with-documentdb
+kubectl delete deployment nodejs-todo-deployment-with-documentdb
