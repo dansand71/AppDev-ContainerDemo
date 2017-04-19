72b0d5e15554abbb8cd424d76e8078094882b990
dansand71 <dansand71@hotmail.com>
2017-04-18 23:24:46 -0400

    Batch stop and start scripts
    Saving money for consumption

--- x-start-all-vms.sh
+++ x-start-all-vms.sh
@@ -10,6 +10,5 @@ for rg in "${rgList[@]}"
 do
     echo -e "\e[33mWorking on rg:$rg"
     ~/bin/az vm list -g $rg -o tsv | for i in $(awk -F$'\t' '{print $8}'); do ~/bin/az vm start -g $rg -n $i -o tsv 2>&1 | tee -a azure.log & done
-    popd
 done
 echo "Complete."
--- x-stop-deallocate-all-vms.sh
+++ x-stop-deallocate-all-vms.sh
@@ -11,7 +11,6 @@ for rg in "${rgList[@]}"
 do
     echo -e "\e[33mWorking on rg:$rg"
     ~/bin/az vm list -g $rg -o tsv | for i in $(awk -F$'\t' '{print $8}'); do ~/bin/az vm deallocate -g $rg -n $i -o tsv 2>&1 | tee -a azure.log & done
-    popd
 done
 echo "Complete."
 
