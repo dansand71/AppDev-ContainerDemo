d6803ddf25a146591998ce693a296198b6557704
dansand71 <dansand71@hotmail.com>
2017-04-19 08:48:29 -0400

    change from https to git://

--- sample-apps/nodejs-todo/demo/1-setup-demo.sh
+++ sample-apps/nodejs-todo/demo/1-setup-demo.sh
@@ -38,7 +38,7 @@ if [ "$(ls -A /source/AppDev-ContainerDemo/sample-apps/nodejs-todo/src)" ]; then
 else
     echo ".source directory is empty.  cloning from github."
     cd /source/AppDev-ContainerDemo/sample-apps/nodejs-todo/src
-    git clone https://github.com/dansand71/node-todo .
+    git clone git://github.com/dansand71/node-todo .
 fi
 
 
