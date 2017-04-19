5eb7d56df9298a7c696896585d7f5a32e2dca3b8
dansand71 <dansand71@hotmail.com>
2017-04-18 23:20:16 -0400

    batch stop and start to save money

---
+++ x-start-all-vms.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rgList=(
+    "ossdemo-appdev-iaas"
+    "ossdemo-appdev-acs"
+    "ossdemo-appdev-oshift"
+)
+
+for rg in "${rgList[@]}"
+do
+    echo -e "\e[33mWorking on rg:$rg"
+    ~/bin/az vm list -g $rg -o tsv | for i in $(awk -F$'\t' '{print $8}'); do ~/bin/az vm start -g $rg -n $i -o tsv 2>&1 | tee -a azure.log & done
+    popd
+done
+echo "Complete."
