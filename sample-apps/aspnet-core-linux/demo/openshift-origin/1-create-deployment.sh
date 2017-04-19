66ec5710459f1c1745a5dfb0699c38646c9c2038
dansand71 <dansand71@hotmail.com>
2017-04-18 23:04:37 -0400

    openshift tweaks

--- sample-apps/aspnet-core-linux/demo/openshift-origin/1-create-deployment.sh
+++ sample-apps/aspnet-core-linux/demo/openshift-origin/1-create-deployment.sh
@@ -1,14 +1,14 @@
 #SSH to master
-ssh VALUEOF-DEMO-ADMIN-USER-NAME@VALUEOF-UNIQUE-SERVER-PREFIX-ossdemo-oshift-master.eastus.cloudapp.azure.com
-
+ssh VALUEOF-DEMO-ADMIN-USER-NAME@VALUEOF-UNIQUE-SERVER-PREFIX-ossdemo-oshift-master.eastus.cloudapp.azure.com "
+echo 'Logging into Openshift'
 oc login
 #oc create -n openshift -f https://raw.githubusercontent.com/redhat-developer/s2i-dotnetcore/master/templates/dotnet-example.json
-
+echo 'Creating new project'
 #Create new project
 oc new-project aspnet-core-linux
 oc project aspnet-core-linux
 oc status
-
+echo 'Creating secret if it doesnt already exist'
 #Create Secret for logging into the Azure Registry - for use if you want to show the image pull vs S2I demos
 oc secrets new-dockercfg ossdemoregistrykey \
         --docker-server=VALUEOF-REGISTRY-SERVER-NAME \
@@ -17,15 +17,24 @@ oc secrets new-dockercfg ossdemoregistrykey \
         --docker-email=GBBOSS@microsoft.com
 
 oc secrets link default ossdemoregistrykey --for=pull
+echo 'Updating security policy'
+#https://docs.openshift.org/latest/admin_guide/manage_scc.html#enable-images-to-run-with-user-in-the-dockerfile
+oadm policy add-scc-to-group anyuid system:authenticated
 
+echo 'Creating new app with image as the base'
 #Image app example
-#oc new-app --docker-image=VALUEOF-REGISTRY-SERVER-NAME/ossdemo/aspnet-core-linux:latest --insecure-registry=true
+oc new-app --docker-image=VALUEOF-REGISTRY-SERVER-NAME/ossdemo/aspnet-core-linux:latest --insecure-registry=true --name=aspnet-image
+
+#expose app
+echo 'Exposing a default route to the application'
+oc expose service aspnet-image
 
 
 #S2I app example
 #https://github.com/openshift-s2i/s2i-aspnet    
 #oc new-build https://github.com/openshift-s2i/s2i-aspnet
-oc new-app registry.access.redhat.com/dotnet/dotnetcore-11-rhel7~https://github.com/dansand71/sampleApp-s2i-aspnetcore:dotnetcore-1.1 --name=aspnet-app --context-dir=app
-oc service expose aspnet-app
+#oc new-app registry.access.redhat.com/dotnet/dotnetcore-11-rhel7~https://github.com/dansand71/sampleApp-s2i-aspnetcore:dotnetcore-1.1 --name=aspnet-app --context-dir=app
+#oc service expose aspnet-app
 
+"
 echo "Please login to the Openshift origin cluster: https://VALUEOF-UNIQUE-SERVER-PREFIX-ossdemo-oshift-master.eastus.cloudapp.azure.com:8443 "
