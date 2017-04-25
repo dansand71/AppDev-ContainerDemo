ad7953d1b46c4bf093e389fdc5fb9e90ba900c5d
dansand71 <dansand71@hotmail.com>
2017-04-25 11:48:58 -0400

    ssh

--- sample-apps/aspnet-core-linux/demo/openshift-origin/2-deploy-s2i.sh
+++ sample-apps/aspnet-core-linux/demo/openshift-origin/2-deploy-s2i.sh
@@ -1,6 +1,7 @@
 #SSH to master
 ssh VALUEOF-DEMO-ADMIN-USER-NAME@VALUEOF-UNIQUE-SERVER-PREFIX-ossdemo-oshift-master.eastus.cloudapp.azure.com "
 echo 'Logging into Openshift'
+echo 'ssh VALUEOF-DEMO-ADMIN-USER-NAME@VALUEOF-UNIQUE-SERVER-PREFIX-ossdemo-oshift-master.eastus.cloudapp.azure.com'
 oc login
 oc create -n openshift -f https://raw.githubusercontent.com/redhat-developer/s2i-dotnetcore/master/templates/dotnet-example.json
 echo 'Creating new project'
