f613c01b2ad6beb634cc151d4200da75fd4fdac8
dansand71 <dansand71@hotmail.com>
2017-05-01 18:46:22 -0400

    OMS AGent redeploy with Ansible

--- environment/iaas/redeploy-OMSAgent.sh
+++ environment/iaas/redeploy-OMSAgent.sh
@@ -9,7 +9,6 @@ rgList=(
 for rg in "${rgList[@]}"
 do
     echo -e "\e[33mWorking on rg:$rg"
-    ~/bin/az vm list -g $rg -o tsv | for i in $(awk -F$'\t' '{print $8}'); do sudo /opt/microsoft/omsagent/bin/omsadmin.sh -X & done
-    ~/bin/az vm list -g $rg -o tsv | for i in $(awk -F$'\t' '{print $8}'); do sudo /opt/microsoft/omsagent/bin/omsadmin.sh -w VALUEOF-REPLACE-OMS-WORKSPACE -s VALUEOF-REPLACE-OMS-PRIMARYKEY & done
+    ~/bin/az vm list -g $rg -o tsv | for i in $(awk -F$'\t' '{print $8}'); do ansible-playbook redeploy-OMSAgent-playbook.yml -i $i & done
 done
 echo "Complete."
