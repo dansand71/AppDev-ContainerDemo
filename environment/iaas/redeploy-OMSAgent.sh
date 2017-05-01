cfbb330852d57f7764f8f1a9d0b289aefba6851f
dansand71 <dansand71@hotmail.com>
2017-05-01 18:27:32 -0400

    fix of OMSAgent key
    fix of OMSAgent key

--- environment/iaas/redeploy-OMSAgent.sh
+++ environment/iaas/redeploy-OMSAgent.sh
@@ -10,6 +10,6 @@ for rg in "${rgList[@]}"
 do
     echo -e "\e[33mWorking on rg:$rg"
     ~/bin/az vm list -g $rg -o tsv | for i in $(awk -F$'\t' '{print $8}'); do /opt/microsoft/omsagent/bin/omsadmin.sh -X done
-    ~/bin/az vm list -g $rg -o tsv | for i in $(awk -F$'\t' '{print $8}'); do /opt/microsoft/omsagent/bin/omsadmin.sh -w VALUEOF-REPLACE-OMS-WORKSPACE -s -s VALUEOF-REPLACE-OMS-PRIMARYKEY done
+    ~/bin/az vm list -g $rg -o tsv | for i in $(awk -F$'\t' '{print $8}'); do /opt/microsoft/omsagent/bin/omsadmin.sh -w VALUEOF-REPLACE-OMS-WORKSPACE -s VALUEOF-REPLACE-OMS-PRIMARYKEY done
 done
 echo "Complete."
