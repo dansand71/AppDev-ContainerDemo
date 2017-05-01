dd2f917f541702a2d2abd4b65cdac5e99b02ed80
dansand71 <dansand71@hotmail.com>
2017-05-01 18:59:51 -0400

    small tweak to add hostnames for oms configuration

--- environment/iaas/redeploy-OMSAgent.sh
+++ environment/iaas/redeploy-OMSAgent.sh
@@ -10,11 +10,11 @@ for rg in "${rgList[@]}"
 do
     echo -e "\e[33mWorking on rg:$rg"
     echo -e "reconfiguring oms agents"
-    ~/bin/az vm list -g $rg -o tsv | for i in $(awk -F$'\t' '{print $8}'); do ansible-playbook deploy-OMSAgent-playbook.yml -i $i & done
+    ~/bin/az vm list -g $rg -o tsv | for i in $(awk -F$'\t' '{print $8}'); do ansible-playbook deploy-OMSAgent-playbook.yml -i $i, & done
     
     if [$rg == "ossdemo-appdev-oshift" ] ; then
     echo "installing OMS Agent for the first time to all Openshift agent vm's."
-        ~/bin/az vm list -g $rg -o tsv | for i in $(awk -F$'\t' '{print $8}'); do ansible-playbook redeploy-OMSAgent-playbook.yml -i $i & done
+        ~/bin/az vm list -g $rg -o tsv | for i in $(awk -F$'\t' '{print $8}'); do ansible-playbook redeploy-OMSAgent-playbook.yml -i $i, & done
     fi
     
 done
